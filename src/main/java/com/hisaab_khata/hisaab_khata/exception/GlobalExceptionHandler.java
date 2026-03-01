7f305b54a125d66c388ae8f8745b119224fdffb3
abhishek <abhishek37514@gmail.com>
2026-03-01 11:06:19 +0530

    added modification

--- src/main/java/com/hisaab_khata/hisaab_khata/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hisaab_khata/hisaab_khata/exception/GlobalExceptionHandler.java
@@ -3,7 +3,7 @@ package com.hisaab_khata.hisaab_khata.exception;
 import com.hisaab_khata.hisaab_khata.dto.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
+//import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.security.authentication.BadCredentialsException;
